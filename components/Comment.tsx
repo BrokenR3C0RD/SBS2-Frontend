a308c9e00ff48a942248f7670f6ab0553c352204
MasterR3C0RD <BrokenR3C0RD@users.noreply.github.com>
2020-04-27 04:09:17 -0300

    :crab: Forgot the component :crab:

---
+++ components/Comment.tsx
@@ -0,0 +1,127 @@
+import DisplayMarkup from "./DisplayMarkup";
+import { CRUD } from "../classes/Entity";
+import { Comment, FullUser, Content } from "../classes";
+import Link from "next/link";
+import moment from "moment";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+import { Spinner } from "./Layout";
+import Composer from "./Composer";
+import Form from "./Form";
+
+const Comments = (({
+    parent,
+    reverse = false,
+    self,
+    className = "comments"
+}) => {
+    const [ref, inView] = useInView();
+    const [commentCode, setCommentCode] = useState<string>("");
+    const [commentMarkup, setCommentMarkup] = useState<string>("12y");
+    const [commentId, setCommentId] = useState<number>(0);
+
+    const [rawComments, users, listeners, fetching, loadMore] = Comment.useComments([parent], self != null);
+
+    useEffect(() => {
+        if (inView) {
+            loadMore()
+        }
+    }, [inView]);
+
+    let comments = rawComments.slice();
+    if (reverse)
+        comments = comments.reverse();
+
+
+    async function EditComment(id: number) {
+        let comment = comments.find(comment => comment.id == id);
+
+        setCommentCode(comment?.content?.t || "");
+        setCommentMarkup(comment?.content?.m || "12y");
+        setCommentId(comment?.id || 0);
+    }
+
+    async function PostComment() {
+        await Comment.Update({
+            parentId: parent!.id,
+            id: (commentId === 0 ? undefined : commentId),
+            content: {
+                t: commentCode,
+                m: commentMarkup
+            }
+        });
+
+        setCommentCode("");
+        setCommentMarkup("12y");
+        setCommentId(0);
+    }
+
+    async function DeleteComment(id: number) {
+        if (!confirm("Are you sure you want to delete this comment?"))
+            return;
+
+        await Comment.Delete(comments.find(comment => comment.id == id)!);
+    }
+
+    return <div className={className}>
+        <ul className="comment-listeners">
+            {listeners.map(listener => {
+                let user = users.find(user => user.id == listener);
+                if (user == null) return null;
+
+                return <li key={user.id}>
+                    <Link href="/user/[uid]" as={`/user/${user.id}`}><a><img src={user.GetAvatarURL(64)} className="comment-listener" title={user.username} /></a></Link>
+                </li>
+            })}
+        </ul>
+        {reverse && fetching && <Spinner />}
+        {
+            comments.slice().map((comment, idx) => {
+                let user = users.find(user => user.id == comment.createUserId);
+                if (user == null) return null;
+
+                return <div className="comment" key={comment.id} ref={comments.length - 1 == idx && !fetching ? ref : undefined}>
+                    <img src={user.GetAvatarURL(64)} className="avatar" />
+                    <div className="comment-body">
+                        <div className="user-info">
+                            <span className="username">
+                                <Link href="/user/[uid]" as={`/user/${user.id}`}><a>{user.username}</a></Link>
+                            </span>
+
+                            <span className="editdate">
+                                {((comment.editDate.valueOf() - comment.createDate.valueOf()) >= 2000) ? "Edited " : "Posted "} {moment(comment.editDate).fromNow()}
+                            </span>
+                        </div>
+
+                        <div className="comment-content">
+                            {
+                                comment.id == commentId && <Form onSubmit={PostComment} className="comment-edit">
+                                    <Composer hidePreview code={commentCode} markup={commentMarkup} onChange={(code, markup) => { setCommentCode(code); setCommentMarkup(markup); }} />
+                                    <div className="edit-buttons">
+                                        <button type="button" onClick={() => EditComment(0)}><span className="iconify" data-icon="topcoat:cancel" data-inline="true"></span></button>
+                                        <button type="submit"><span className="iconify" data-icon="mdi:send" data-inline="true"></span></button>
+                                    </div>
+                                </Form> || <DisplayMarkup code={comment.content["t"]} markupLang={comment.content["m"]} />
+                            }
+                        </div>
+                        {self && (comment.Permitted(self, CRUD.Update) || comment.Permitted(self, CRUD.Delete) || self.super) &&
+                            <button className="gear" type="button" onClick={(evt) => evt.currentTarget.dataset.open = evt.currentTarget.dataset.open == "true" ? "false" : "true"}><span className="iconify" data-icon="octicon:gear" data-inline="true"></span></button>
+                        }
+                        <div className="buttons">
+                            {self && comment.Permitted(self, CRUD.Update) && <button type="button" style={{ textAlign: "center" }} onClick={() => EditComment(comment.id)}><span className="iconify" data-icon="fe:pencil" data-inline="true"></span></button>}
+                            {self && comment.Permitted(self, CRUD.Delete) && <button type="button" style={{ color: "lightcoral", }} onClick={() => { DeleteComment(comment.id) }}><span className="iconify" data-icon="ic:baseline-delete" data-inline="true"></span></button>}
+                        </div>
+                    </div>
+                </div>
+            })
+        }
+        {!reverse && fetching && <Spinner />}
+    </div>;
+}) as React.FunctionComponent<{
+    parent: Content,
+    reverse?: boolean,
+    self?: FullUser,
+    className?: string
+}>
+
+export { Comments }

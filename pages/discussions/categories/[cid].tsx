b5e76293fcdfa468040b91fb817ddc406a5925ed
MasterR3C0RD <BrokenR3C0RD@users.noreply.github.com>
2020-04-28 03:04:04 -0300

    Fix discussion permissions

--- pages/discussions/categories/[cid].tsx
+++ pages/discussions/categories/[cid].tsx
@@ -65,6 +65,9 @@ export default (({
             values: {
                 markupLang
             },
+            permissions: {
+                "0": "cr"
+            },
             type: "@discussion"
         });
 
